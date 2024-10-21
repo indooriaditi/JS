04601ac6612264eb0aea54b664048c6aa92929d0
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-10-20 19:53:26 -0700

    variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,13 @@
 //import { apiKey, abc as content } from "./util.js"
 //import * as util from "./util.js"
 
-import apiKey from "./util.js";
+// import apiKey from "./util.js";
 
-console.log(apiKey);
+// can reassign
+let userMessage = "Hello, world";
+userMessage = "Hi!"
+
+// can't reassign
+const name = "aditi";
+
+console.log(userMessage);
