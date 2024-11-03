1a3b0667ef246cbb13db094cb81de8bcc58f81a8
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-11-03 11:52:30 -0800

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,12 +9,21 @@
 // can reassign
 // let userMessage = "Hello, world";
 // userMessage = "Hi!"
+// console.log(userMessage);
 
 // can't reassign
 // const name = "aditi";
 
 // [operators]
-console.log("hello"+"world");
-console.log(3===4);
+// console.log("hello"+"world");
+// console.log(3===4);
 
-console.log(userMessage);
+//[functions]
+function greet(userName, message) {
+    // console.log(userName);
+    // console.log(message);
+    return userName + " " + message;
+}
+
+const greeting = greet("Aditi","Hello"); // because function is returning something
+console.log(greeting);
